77aca0eaa292015a093f5f76a4f390436b8b736d
Neo-X <glen.berseth@mila.quebec>
2025-07-07 20:34:02 -0400

    Fixes for launching jobs on mila cluster.

---
+++ launchGPU.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+## Run with sbatch --array=1-3 launch.sh
+
+#SBATCH --partition=long                                # Ask for unkillable job
+#SBATCH --cpus-per-task=8                               # Ask for 2 CPUs
+#SBATCH --ntasks=1                                      # Ask for 2 tasks per GPU 
+#SBATCH --gres=gpu:1                                    # Ask for GPUs
+#SBATCH --mem=64G                                       # Ask for 10 GB of RAM
+#SBATCH --time=7:55:00                                # The job will run for 3 hours
+#SBATCH -o /network/scratch/g/glen.berseth/slurm-%j.out  # Write the log on scratch
+#SBATCH --no-requeue                                 # Do not requeue the job if it fails
+
+module load cudatoolkit/12.1 miniconda/3 cuda/12.1.1/cudnn/9.3
+conda activate roble
+
+## srun is needed to run the multiple tasks per GPU
+python mini-grp2.py r_seed=$SLURM_ARRAY_TASK_ID testing=false $ARGSS
---
+++ run_jobs_list.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+## Code to run all the jobs for this codebase 
+
+## Atari RL envs
+strings=(
+    # "MontezumaRevengeNoFrameskip-v4"
+    # "AsterixNoFrameskip-v4"
+    "SpaceInvadersNoFrameskip-v4"
+    # "PitfallNoFrameskip-v4"
+    # "PhoenixNoFrameskip-v4"
+)
+for env in "${strings[@]}"; do
+    # echo "$env"
+    sbatch --array=1-3 --export=ALL,ENV_ID=$env,ARGSS='max_iters=50000' launchGPU.sh
+done
